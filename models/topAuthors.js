ccc0f991c9af457a11209c78440bc300b232dba3
dawood-dev <masslawi@gmail.com>
2021-10-31 23:27:38 -0700

    Models.

---
+++ models/topAuthors.js
@@ -0,0 +1,12 @@
+let topAuthor = {
+    name: {
+        type: String,
+        required: true
+    },
+    commits: {
+        type: Number,
+        required: true
+    }
+};
+
+module.exports = topAuthor;
---
+++ models/user.js
@@ -0,0 +1,12 @@
+let User = {
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    }
+};
+
+module.exports = User;
